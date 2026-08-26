a58a89c333e4aa7501b1b4748ee93d892b9f7044
Marcin Matras <marcin.matras@rtbhouse.com>
2026-08-26 19:15:56 +0200

    implement galery 2

---
+++ src/controller/galleryController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+import { PhotoModel } from '../model/Photo';
+
+export const getGallery = async (req: Request, res: Response) => {
+  const category = (req.query.category as string) || 'standing';
+  const photos = await PhotoModel.getByCategory(category);
+  
+  // Renderowanie widoku głównego z przekazaniem zdjęć i aktualnej kategorii
+  res.render('gallery', { photos, currentCategory: category });
+};
+
+export const getAddPhotoForm = (req: Request, res: Response) => {
+  res.render('add-photo');
+};
+
+export const postAddPhoto = async (req: Request, res: Response) => {
+  const { galeria, komentarz } = req.body;
+  const filename = req.file?.filename; // Nazwa zapisanego pliku przez Multer
+
+  if (filename) {
+    await PhotoModel.addPhoto(filename, galeria, komentarz);
+  }
+
+  res.redirect(`/?category=${galeria}`);
+};
